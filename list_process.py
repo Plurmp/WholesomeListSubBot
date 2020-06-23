885441710c9ec00fa33ce60732dc11c8db23ce92
plurmp <plurmpmcflurnt@gmail.com>
2020-06-22 21:14:24 -0500

    fixed format of local.json dict

--- list_process.py
+++ list_process.py
@@ -3,18 +3,18 @@ import json
 with open("local_list.json", encoding='utf-8') as file:
     god_list = json.load(file)
 
+local_list = {}
+
 with open('local.json', 'w') as w:
     for item in god_list['table']:
         print(type(item))
-        json.dump(
-            {item['link']: {
-                'title': item['title'],
-                'author': item['author'],
-                'warning': item['warning'],
-                'parody': item['parody'],
-                'tier': item['tier'],
-                'pages': item['pages'],
-                'tags': item['tags'],
-            }},
-            fp=w
-        )
+        local_list[item['link']] = {
+            'title': item['title'],
+            'author': item['author'],
+            'warning': item['warning'],
+            'parody': item['parody'],
+            'tier': item['tier'],
+            'pages': item['pages'],
+            'tags': item['tags']
+        }
+    json.dump(local_list, w)
